58750f5fa4b689520f3693fa236638f8739592e4
Mickle <mickle@indiro.ru>
2020-08-08 00:29:54 +0300

    test2

--- routes/dev/links.js
+++ routes/dev/links.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/links', (req, res) => {
-    res.send('Short links! This is test');
+    res.send('Short links! This is test end');
 });
 
 module.exports = router;
