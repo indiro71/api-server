ebe4c4f3efd4f1c703bf66f93d1a666c9c8e978a
Mickle <git@indiro.ru>
2021-03-08 20:14:41 +0300

    update functions

--- helpers/api/getWeather.js
+++ helpers/api/getWeather.js
@@ -1,15 +1,26 @@
 const { getData } = require('../getData');
-const keys = require("../../keys");
-const apiUrls = require("./apiUrls");
+const keys = require('../../keys');
+const apiUrls = require('./apiUrls');
 
 module.exports = {
-    async getWeather(ip) {
+    async getWeatherByIp(ip) {
         try {
             if (!ip) return undefined;
-            const { latitude, longitude } = await getData(`${apiUrls.GEO_SERVICE_URL}${ip}?access_key=${keys.LOCATION_API_KEY}`);
+            const {
+                latitude,
+                longitude
+            } = await getData(`${apiUrls.GEO_SERVICE_URL}${ip}?access_key=${keys.LOCATION_API_KEY}`);
             return await getData(`${apiUrls.WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${keys.WEATHER_API_KEY}&units=metric`);
         } catch (e) {
-            console.log();
+            console.log(e);
         }
-    }
-}
+    },
+    async getWeatherByCoords(lat, lon) {
+        try {
+            if (!lat || !lon) return undefined;
+            return await getData(`${apiUrls.WEATHER_URL}?lat=${lat}&lon=${lon}&appid=${keys.WEATHER_API_KEY}&units=metric`);
+        } catch (e) {
+            console.log(e);
+        }
+    },
+};
