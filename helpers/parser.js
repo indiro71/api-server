d990e7e5d78257bc30c89ec426631993b515e72e
Mickle <git@indiro.ru>
2021-03-09 00:41:49 +0300

    update

--- helpers/parser.js
+++ helpers/parser.js
@@ -6,19 +6,15 @@ const LAUNCH_PUPPETEER_OPTS = {
         '--no-sandbox',
         '--window-size=320,570',
         '--lang=en-EN,en',
-        // '--proxy-server=84.17.51.212:3128'
     ]
 };
 
 class Parser {
     constructor() {
         this.browser = null;
-        this.page = null;
     }
     async initBrowser() {
         this.browser = await puppeteer.launch(LAUNCH_PUPPETEER_OPTS);
-        this.page = await this.browser.newPage();
-        await this.page.setUserAgent('Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36');
     }
     closeBrowser() {
         this.browser.close();
@@ -29,8 +25,12 @@ class Parser {
         }
 
         try {
-            await this.page.goto(url, { waitUntil: 'domcontentloaded' });
-            let content = await this.page.content();
+            const page = await this.browser.newPage();
+            await page.setUserAgent('Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36');
+            // await page.goto(url, { waitUntil: 'load' });
+            await page.goto(url, { waitUntil: 'domcontentloaded' });
+            const content = await page.content();
+            await this.closeBrowser();
             return content;
         } catch (err) {
             throw err;
