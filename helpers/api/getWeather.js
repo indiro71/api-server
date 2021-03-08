b4d6ce830708c5fd91835b92159b586603cb8dcd
Mickle <git@indiro.ru>
2021-03-09 00:41:06 +0300

    Revert "update functions"
    This reverts commit ebe4c4f3

--- helpers/api/getWeather.js
+++ helpers/api/getWeather.js
@@ -1,26 +1,15 @@
 const { getData } = require('../getData');
-const keys = require('../../keys');
-const apiUrls = require('./apiUrls');
+const keys = require("../../keys");
+const apiUrls = require("./apiUrls");
 
 module.exports = {
-    async getWeatherByIp(ip) {
+    async getWeather(ip) {
         try {
             if (!ip) return undefined;
-            const {
-                latitude,
-                longitude
-            } = await getData(`${apiUrls.GEO_SERVICE_URL}${ip}?access_key=${keys.LOCATION_API_KEY}`);
+            const { latitude, longitude } = await getData(`${apiUrls.GEO_SERVICE_URL}${ip}?access_key=${keys.LOCATION_API_KEY}`);
             return await getData(`${apiUrls.WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${keys.WEATHER_API_KEY}&units=metric`);
         } catch (e) {
-            console.log(e);
+            console.log();
         }
-    },
-    async getWeatherByCoords(lat, lon) {
-        try {
-            if (!lat || !lon) return undefined;
-            return await getData(`${apiUrls.WEATHER_URL}?lat=${lat}&lon=${lon}&appid=${keys.WEATHER_API_KEY}&units=metric`);
-        } catch (e) {
-            console.log(e);
-        }
-    },
-};
+    }
+}
